2929a0d25733cc9972df471166de75164383b79b
Fkhrayef <alkhrayefff@gmail.com>
2025-07-04 15:29:41 +0300

    print board method

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,21 @@ public class Main {
                 {'7', '8', '9'}
         };
 
+        printBoard(board);
+    }
 
+    public static void printBoard(char[][] board) {
+        System.out.println();
+        for (int i = 0; i < board.length; i++) {
+            System.out.print(" ");
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j]);
+                if (j < 2) System.out.print(" │ ");
+            }
+            System.out.println();
+            if (i < 2) System.out.println("───┼───┼───");
+        }
+        System.out.println();
     }
 
 }
