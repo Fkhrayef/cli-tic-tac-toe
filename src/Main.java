d8e9938a2c53cd8ee79c670c62b4a3e63c9961ea
Fkhrayef <alkhrayefff@gmail.com>
2025-07-04 15:27:52 +0300

    welcome message & initialize board

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(
+                        "┌──────────────────────────────┐\n" +
+                        "│                              │\n" +
+                        "│  Welcome to CLI-Tic-Tac-Toe  │\n" +
+                        "│    Java Bootcamp Project1    │\n" +
+                        "│      Made by Fkhrayef        │\n" +
+                        "│                              │\n" +
+                        "└──────────────────────────────┘\n");
+
+        char[][] board = {
+                {'1', '2', '3'},
+                {'4', '5', '6'},
+                {'7', '8', '9'}
+        };
+
+
+    }
+
+}
